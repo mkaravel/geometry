14bfcca3544a96a3f14d8beff5e779d444601602
Menelaos Karavelas <menelaos.karavelas@gmail.com>
2014-03-18 10:16:23 +0200

    [geometry][distance] removed code not using comparable distances; re-implemented the inner dispatch using the spitted_dispatch namespace;

--- include/boost/geometry/algorithms/detail/distance/segment_to_box.hpp
+++ include/boost/geometry/algorithms/detail/distance/segment_to_box.hpp
@@ -34,12 +34,12 @@ template
     typename Box,
     typename Strategy
 >
-struct distance_segment_box_2D
+class distance_segment_box_2D
 {
+private:
     typedef typename point_type<Segment>::type SegmentPoint;
     typedef typename point_type<Box>::type BoxPoint;
 
-#ifdef BOOST_GEOMETRY_USE_COMPARABLE_DISTANCES
     typedef typename strategy::distance::services::comparable_type
         <
             Strategy
@@ -50,30 +50,19 @@ struct distance_segment_box_2D
             Strategy
         > GetComparable;
 
-    typedef typename strategy::distance::services::return_type
-        <
-            ComparableStrategy, SegmentPoint, BoxPoint
-        >::type return_type;
-
-
-
     typedef typename strategy::distance::services::strategy_point_point
         <
             ComparableStrategy
         >::type pp_strategy_type;
-#else
-    typedef typename strategy::distance::services::return_type
-        <
-            Strategy, SegmentPoint, BoxPoint
-        >::type return_type;
-
-    typedef typename strategy::distance::services::strategy_point_point
-        <
-            Strategy
-        >::type pp_strategy_type;
-#endif
 
     typedef point_to_box<SegmentPoint, Box, pp_strategy_type> PointToBox;
+
+
+public:
+    typedef typename strategy::distance::services::return_type
+        <
+            ComparableStrategy, SegmentPoint, BoxPoint
+        >::type return_type;
 
 
     static inline return_type apply(Segment const& segment,
@@ -95,7 +84,6 @@ struct distance_segment_box_2D
         d[0] = PointToBox::apply(p[0], box, pp_strategy);
         d[1] = PointToBox::apply(p[1], box, pp_strategy);
 
-#ifdef BOOST_GEOMETRY_USE_COMPARABLE_DISTANCES
         ComparableStrategy cstrategy = GetComparable::apply(strategy);
 
         d[2] = cstrategy.apply(box.min_corner(), p[0], p[1]);
@@ -121,61 +109,8 @@ struct distance_segment_box_2D
                 Segment,
                 Box
             >::apply( *std::min_element(d, d+6) );
-#else
-        d[2] = strategy.apply(box.min_corner(), p[0], p[1]);
-        d[3] = strategy.apply(box.max_corner(), p[0], p[1]);
-
-        BoxPoint top_left, bottom_right;
-
-        geometry::assign_values(top_left,
-                                geometry::get<0>(box.min_corner()),
-                                geometry::get<1>(box.max_corner()));
-
-        geometry::assign_values(bottom_right,
-                                geometry::get<0>(box.max_corner()),
-                                geometry::get<1>(box.min_corner()));
-
-        d[4] = strategy.apply(top_left, p[0], p[1]);
-        d[5] = strategy.apply(bottom_right, p[0], p[1]);
-
-        return *std::min_element(d, d+6);
-#endif
     }
 };
-
-
-
-template
-<
-    typename Segment,
-    typename Box,
-    std::size_t DimensionCount,
-    typename Strategy
->
-struct segment_to_box_dispatch
-    : not_implemented<Segment, Box, Strategy>
-{};
-
-
-template
-<
-    typename Segment,
-    typename Box,
-    typename Strategy
->
-struct segment_to_box_dispatch<Segment, Box, 2, Strategy>
-    : distance_segment_box_2D<Segment, Box, Strategy>
-{};
-
-
-template <typename Segment, typename Box, typename Strategy>
-struct segment_to_box
-    : segment_to_box_dispatch
-        <
-            Segment, Box, dimension<Segment>::type::value, Strategy
-        >
-{};
-
 
 
 }} // namespace detail::distance
@@ -186,6 +121,37 @@ struct segment_to_box
 namespace dispatch
 {
 
+namespace splitted_dispatch
+{
+
+
+template
+<
+    typename Segment,
+    typename Box,
+    std::size_t SegmentDimension,
+    std::size_t BoxDimension,
+    typename Strategy
+>
+struct distance_segment_to_box
+    : not_implemented<Segment, Box, Strategy>
+{};
+
+
+template
+<
+    typename Segment,
+    typename Box,
+    typename Strategy
+>
+struct distance_segment_to_box<Segment, Box, 2, 2, Strategy>
+    : detail::distance::distance_segment_box_2D<Segment, Box, Strategy>
+{};
+
+
+} // namespace splitted_dispatch
+
+
 // segment-box
 template <typename Segment, typename Box, typename Strategy>
 struct distance
@@ -193,7 +159,14 @@ struct distance
         Segment, Box, Strategy, segment_tag, box_tag,
         strategy_tag_distance_point_segment, false
     >
-    : detail::distance::segment_to_box<Segment, Box, Strategy>
+    : splitted_dispatch::distance_segment_to_box
+        <
+            Segment,
+            Box,
+            dimension<Segment>::type::value,
+            dimension<Box>::type::value,
+            Strategy
+        >
 {};
 
 
